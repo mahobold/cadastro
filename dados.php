0204c108a712dfb2595fe50fea604579fde6b12b
MARIA VITORIA MAGALHAES HOBOLD <hobold.7994@aluno.pr.senac.br>
2023-03-24 08:13:56 -0300

    include

--- dados.php
+++ dados.php
@@ -9,30 +9,9 @@
         <title>Dados recebidos</title>
     </head>
     <body>
-        <nav class="navbar bg-body-tertiary fixed-top">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Cadastro</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="cadastro.php">Cadastro</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="dados.php">Dados</a>
-                    </li>
-                    </ul>
-                </div>
-                </div>
-            </div>
-        </nav>
+         <?php
+            include ("menu.php");
+        ?>
         <div class="container"></div>
             <h1>Dados recebidos :</h1>
             <p><b> O nome recebido foi: <b><?php echo $_GET ["nome"]; ?> </p>
